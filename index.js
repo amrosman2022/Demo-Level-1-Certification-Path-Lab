fc0fbbc78b785ca39a651d03c60b63de939401d9
Amr Osman <99691484+amrosman2022@users.noreply.github.com>
2022-02-28 17:18:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const uuid = require('uuid/v4')
+const LaunchDarkly = require('launchdarkly-node-server-sdk')
+const port = 8080
+
+const ldClient = LaunchDarkly.init("sdk-4de14f35-6483-4580-961d-96dca749ce0e")
+
+const user = {
+    "firstName":"Super",
+    "lastName":"Coolguy",
+    "key": uuid(),
+    "email":"SuperCoolguy@example.com",
+    "custom":{
+       "groups":"beta_testers"
+    }
+  };
+
+  // this is for the HTTP request
+//app.get('/', async (req,res) => {
+ //   let variation = await ldClient.variation("flag1", user, false)
+//    res.send(`${variation}`)
+//})
+
+//app.listen(port, () => {
+//    console.log(`workramp-featureflag-example listening at http://localhost:${port}`)
+//})
+//--------------------------------
+
+// this is for the event subscription 
+ldClient.on('update', (param) => 
+{
+    console.log('a flag was changed: ' + param.key);
+    if (param.key=='flag1')
+    {
+        console.log('The code to be executed when flag1 changes');
+    }
+    if (param.key=='flag2')
+    {
+        console.log('The code to be executed when flag2 changes');
+    }
+});
+  //ldClient.on('update:flag2', () => {
+  //  console.log('the flag2 flag was changed');
+  //});
+  //--------------------------------
